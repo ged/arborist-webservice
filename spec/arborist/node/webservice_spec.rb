75cac90ffea5daec8fc5dae315a011b9f30ac3b2
Michael Granger <ged@FaerieMUD.org>
2016-02-08 10:28:00 -0800

    Initial commit.

---
+++ spec/arborist/node/webservice_spec.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env rspec -cfd
+#encoding: utf-8
+
+require_relative '../../spec_helper'
+
+require 'rspec'
+require 'arborist/node/webservice'
+
+describe Arborist::Node::Webservice do
+
+	
+
+end
+
